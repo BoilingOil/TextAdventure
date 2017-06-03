a27fad27ec602873d47b4e1921ce742ed631074c
Jason Walker <jason@nothingordouble.com>
2017-06-03 14:26:02 -0400

    Initial Commit

---
+++ Assets/TextController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			text.text = "Space key pressed";
+		} else if (Input.GetKey(KeyCode.UpArrow)) {
+			text.text = "Get On Up!";
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			text.text = "The Enemy's Gate is DOWN!!";
+		}
+	}
+}
